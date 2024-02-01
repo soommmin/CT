77cb85e4d71a48c7a21ef0de936808ac66308f51
soommmin <tnakszla@gmail.com>
2024-02-02 01:43:31 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin=open("input.txt", "r")
+from collections import deque
+import heapq as hq
+import itertools as it
+import hashlib
+import copy 
+
+
+print("github test commit!")
