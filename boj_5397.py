2e3d4edc8b86258032745e3c18fefe2fcf1b0437
soommmin <tnakszla@gmail.com>
2024-02-06 21:44:46 +0900

    백준_5397

---
+++ boj_5397.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin=open("input.txt", "r")
+from collections import deque
+import heapq as hq
+import itertools as it
+import hashlib
+import copy 
+
+
+#백준_5397#
+
+test_case = int(input())
+
+for _ in range(test_case):
+    left_stack = []
+    right_stack = []
+    data = input()
+
+    for i in data:
+        if i == '-':
+            if left_stack:
+                left_stack.pop()
+
+        elif i == '<':
+            if left_stack:
+                right_stack.append(left_stack.pop())
+
+        elif i == '>':
+            if right_stack:
+                left_stack.append(right_stack.pop())
+
+        else:
+            left_stack.append(i)
+
+    left_stack.extend(reversed(right_stack))
+    print(''.join(left_stack))
