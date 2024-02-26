81961ca3910ad3a0eddd794b5fb3d52a3ae16b7b
{soommmin} <tnakszla@gmail.com>
2024-02-26 13:37:54 +0900

    섹션5_후위식 연산

---
+++ 알고리즘/part5_04.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin=open("input.txt", "r")
+from collections import deque
+import heapq as hq
+import itertools as it
+import hashlib
+import copy 
+
+
+
+# 후위식 연산
+a = input()
+stack =[]
+for x in a:
+    if x.isdecimal():
+        stack.append(int(x))
+    else:
+        if x=='+':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2+n1)
+        elif x=='-':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2-n1)
+        elif x=='*':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2*n1)
+        elif x=='/':
+            n1=stack.pop()
+            n2=stack.pop()
+            stack.append(n2/n1)
+print(stack[0])
