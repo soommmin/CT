feede21f7133cfc6c30aa0e45abb49d5205dfccb
{soommmin} <tnakszla@gmail.com>
2024-03-02 23:08:19 +0900

    백준_1302

---
+++ boj_1302.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin=open("input.txt", "r")
+from collections import deque
+import heapq as hq
+import itertools as it
+import hashlib
+import copy 
+
+
+#백준_1302#
+
+n = int(input())
+books = {}
+
+for _ in range(n):
+    book = input()
+    if book not in books:
+        books[book] = 1 
+
+    else:
+        books[book] += 1 
+
+target = max(books.values())
+array = []
+
+for book, number in books.items():
+    if number == target:
+        array.append(book)
+
+print(sorted(array)[0])
+
