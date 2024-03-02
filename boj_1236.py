69f905f1ac4e5efcf216fe5589d2c61660d27df1
{soommmin} <tnakszla@gmail.com>
2024-03-02 23:16:37 +0900

    백준_1236

---
+++ boj_1236.py
@@ -0,0 +1,37 @@
+import sys
+sys.stdin=open("input.txt", "r")
+from collections import deque
+import heapq as hq
+import itertools as it
+import hashlib
+import copy 
+
+
+#백준_1236#
+
+n,m = map(int,input().split())
+array = []
+
+for _ in range(n):
+    array.append(input())
+
+row = [0]*n
+column = [0]*m 
+
+for i in range(n):
+    for j in range(m):
+        if array[i][j] == 'X':
+            row[i] = 1 
+            column[j] = 1 
+
+row_count = 0 
+for i in range(n):
+    if row[i] == 0:
+        row_count +=1 
+
+column_count = 0 
+for j in range(m):
+    if column[j] == 0:
+        column_count +=1 
+
+print(max(row_count, column_count))
