71b596647f41ab6ed633042def3f9d30938abc2d
{soommmin} <tnakszla@gmail.com>
2024-04-02 00:08:48 +0900

    백준_1991

---
+++ boj_1991.py
@@ -0,0 +1,56 @@
+import sys
+sys.stdin=open("input.txt", "r")
+from collections import deque
+import heapq as hq
+import itertools as it
+import hashlib
+import copy 
+
+
+#백준_1991#
+
+class Node:
+    def __init__(self, data, left_node, right_node):
+        self.data = data 
+        self.left_node = left_node 
+        self.right_node = right_node 
+
+
+def pre_order(node):
+    print(node.data , end = '')
+    if node.left_node != '.':
+        pre_order(tree[node.left_node])
+    if node.right_node != '.':
+        pre_order(tree[node.right_node])
+
+
+def in_order(node):
+    if node.left_node != '.':
+        in_order(tree[node.left_node])
+    print(node.data, end = '')
+    if node.right_node != '.':
+        in_order(tree[node.right_node])
+
+
+def post_order(node):
+    if node.left_node != '.':
+        post_order(tree[node.left_node])
+
+    if node.right_node != '.':
+        post_order(tree[node.right_node])
+    print(node.data, end = '')
+
+
+n = int(input())
+tree = {}
+
+for i in range(n):
+    data, left_node , right_node = input().split()
+    tree[data] = Node(data, left_node, right_node)
+
+
+pre_order(tree['A'])
+print()
+in_order(tree['A'])
+print()
+post_order(tree['A'])
